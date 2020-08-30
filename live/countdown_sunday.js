0e8613e61a557da0795aef21e315625226aa1d2e
Cambell <cambell.prince@gmail.com>
2020-08-30 20:28:08 +0700

    countdown
    - centered
    - dots on expiry

--- live/countdown_sunday.js
+++ live/countdown_sunday.js
@@ -1,44 +1,40 @@
 function nextDay(date, dow) {
-  date.setDate(date.getDate() + (dow + (7 - date.getDay())) %7);
+  date.setDate(date.getDate() + (dow + (7 - date.getDay())) % 7);
   return date;
 }
 
-function startCountdown(end, display) {
+function startCountdown(end, display, options) {
   var _second = 1000;
   var _minute = _second * 60;
   var _hour = _minute * 60;
   var _day = _hour * 24;
+  var dotCounter = 1;
   var timer = setInterval(function () {
     var now = new Date();
     var distance = end - now;
     if (distance < 0) {
-      clearInterval(timer);
-      display.textContent = '00:00:00';
+      // clearInterval(timer);
+      var dots = '';
+      for (let index = 0; index < dotCounter; index++) {
+        dots += '.';
+      }
+      if (++dotCounter > options.dotCount) {
+        dotCounter = 1;
+      }
+      display.textContent = dots;
+    } else {
+      var days = Math.floor(distance / _day);
+      var hours = Math.floor((distance % _day) / _hour);
+      var minutes = Math.floor((distance % _hour) / _minute);
+      var seconds = Math.floor((distance % _minute) / _second);
+
+      if (days > 0) {
+        display.textContent = days + " days " + hours + ":" + ('0' + minutes).slice(-2) + ":" + ('0' + seconds).slice(-2);
+      } else {
+        display.textContent = hours + ":" + ('0' + minutes).slice(-2) + ":" + ('0' + seconds).slice(-2);
+      }
     }
-    var days = Math.floor(distance / _day);
-    var hours = Math.floor((distance % _day) / _hour);
-    var minutes = Math.floor((distance % _hour) / _minute);
-    var seconds = Math.floor((distance % _minute) / _second);
 
-    display.textContent = hours + ":" + ('0' + minutes).slice(-2) + ":" + ('0' + seconds).slice(-2);
-
-  }, 1000);
-}
-
-function startTimer(duration, display) {
-  var timer = duration, minutes, seconds;
-  setInterval(function () {
-    minutes = parseInt(timer / 60, 10)
-    seconds = parseInt(timer % 60, 10);
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    display.textContent = minutes + ":" + seconds;
-
-    if (--timer < 0) {
-      timer = duration;
-    }
   }, 1000);
 }
 
@@ -48,7 +44,6 @@ window.onload = function () {
   date.setHours(options.startHour, options.startMinute);
   var then = nextDay(date, options.dayOfWeek);
   console.log(then);
-  var fiveMinutes = 60 * 5;
   var display = document.querySelector('#countdown');
-  startCountdown(then, display);
+  startCountdown(then, display, options.countdown || { dotCount: 9 });
 };
